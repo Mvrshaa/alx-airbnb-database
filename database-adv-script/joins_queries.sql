d04474a50dd5f788688bbf84c5855d615dc0bc36
Mvrshaa <mvrshaa@gmail.com>
2025-05-16 22:31:22 +0200

    Complex Queries with Joins

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -15,14 +15,14 @@ INNER JOIN users u ON b.user_id = u.users_id;
 -- LEFT JOIN: List all properties and any reviews they may have
 -- Retrieve all properties and their reviews (if any), including properties with no reviews
 
-SELECT
-  p.property_id,
-  p.property_name,
-  r.review_id,
-  r.review_rating
-FROM
-  properties p
-LEFT JOIN review r ON p.property_id = r.property_id;
+SELECT 
+    p.property_id,
+    p.property_name,
+    r.review_id,
+    r.rating
+FROM 
+    properties p
+LEFT JOIN reviews r ON p.property_id = r.property_id;
 
 -- FULL OUTER JOIN: Include all users and all bookings, even unmatched/not linked
 
